d7061e5e38f009658badf2af9540569bf54759e7
sadC357 <sadhikac357@gmail.com>
2024-01-11 12:52:15 -0600

    learned about styles and text in app.js

--- firstApp/App.js
+++ firstApp/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>hello</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#8ac31d',
     alignItems: 'center',
-    justifyContent: 'center',
+    //justifyContent: 'center',
+    paddingTop:30,
+  },
+  text:{
+    fontSize:70,
+    color:'blue',
   },
 });
