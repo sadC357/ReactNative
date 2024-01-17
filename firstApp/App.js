f5d371b93af7746736768293445d1a32e4238208
sadC357 <sadhikac357@gmail.com>
2024-01-17 12:50:35 -0600

    learned about styles and view

--- firstApp/App.js
+++ firstApp/App.js
@@ -1,25 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 export default function App() {
+  //have javascript
+  const {height, width} = Dimensions.get('window');
+	console.log("height " + height + " width " + width);
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'purple',
+      alignItems: 'center',
+      //justifyContent: 'center',
+      paddingTop:30,
+    },
+    textContainer:{
+      backgroundColor:'lightgrey',
+      width:width*.85,//width=85% of the screen
+    },
+    text:{
+      fontSize:60,
+      color:'blue',
+    },
+  });
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>hello</Text>
+      <View style={styles.textContainer}>
+        {/* comment */}
+        <Text style={styles.text}>HELLO</Text>
+        <Text style={{color:'red',fontSize:20}}>It's very cold</Text>
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#8ac31d',
-    alignItems: 'center',
-    //justifyContent: 'center',
-    paddingTop:30,
-  },
-  text:{
-    fontSize:70,
-    color:'blue',
-  },
-});
